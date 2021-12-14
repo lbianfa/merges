a34479f4928a99dcf2794a3bc7cc3fbcbd70f90f
Fabian Serna <fabianserna2013@gmail.com>
2021-12-13 20:29:32 -0500

    Add main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+class Estudiante {
+
+    constructor(nombre, apellido, edad) {
+        // atributos
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+    }
+
+    saludar() {
+
+    }
+
+    leer(libro) {
+        
+    }
+
+}
